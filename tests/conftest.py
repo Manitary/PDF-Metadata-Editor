cd9fdc9d3f83348b2a8d81dcba025c57ab50e70f
Manitary <pak.ska@gmail.com>
2023-01-31 22:49:06 +0000

    Yield a copy of sample files instead of the files
    Adjust sandbox tests accordingly

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
+"""Fixtures."""
+
 from typing import Generator
 from pathlib import Path
+import shutil
 import pytest
-
+from pytestqt import qtbot
+from pdfMetadataEditor import editor
 
 TEST_ROOT = Path(__file__).parent.resolve()
 PROJECT_ROOT = TEST_ROOT.parent
@@ -9,6 +13,16 @@ SAMPLE_ROOT = PROJECT_ROOT / "samples"
 
 
 @pytest.fixture
-def empty_pdf() -> Generator[Path, None, None]:
+def base_pdf(tmp_path) -> Generator[Path, None, None]:
     """Empty pdf file."""
-    yield SAMPLE_ROOT / "minimal-document.pdf"
+    copy_path = shutil.copy2(
+        SAMPLE_ROOT / "minimal-document.pdf", tmp_path / "file.pdf"
+    )
+    yield copy_path
+
+
+@pytest.fixture
+def new_window(qtbot) -> editor.MainWindow:
+    """A new window of the application."""
+    window = editor.MainWindow()
+    yield window
--- tests/test_generic.py
+++ tests/test_generic.py
@@ -7,17 +7,19 @@ def test_trivial() -> None:
     assert True
 
 
-def test_base_file(tmp_path, empty_pdf) -> None:
-    file_name = os.path.join(tmp_path, "file.pdf")
-    shutil.copy2(empty_pdf, file_name)
-    file_reader = PdfReader(file_name)
-    assert os.path.isfile(file_name)
+def test_base_file(empty_pdf) -> None:
+    file_reader = PdfReader(empty_pdf)
+    assert os.path.isfile(empty_pdf)
     metadata = file_reader.metadata
     assert metadata["/Creator"] == "TeX"
-    with open(file_name, "rb+") as f:
+    with open(empty_pdf, "rb+") as f:
         file_merger = PdfMerger()
         file_merger.append(f)
         file_merger.add_metadata({"/Author": "Test_Author", "/Title": "Test_Title"})
         file_merger.write(f)
-    file_reader = PdfReader(file_name)
+    file_reader = PdfReader(empty_pdf)
     assert file_reader.metadata["/Author"] == "Test_Author"
+
+
+def test_new_window(new_window) -> None:
+    assert new_window
