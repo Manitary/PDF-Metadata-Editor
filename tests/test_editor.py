d80a6fad84d2649f0f2603ef5567f4dd0f7bf49c
Manitary <pak.ska@gmail.com>
2023-02-04 18:41:16 +0000

    Fix tests

--- tests/test_editor.py
+++ tests/test_editor.py
@@ -10,7 +10,7 @@ PASSWORD_BOTH = "foo"
 def test_open_unencrypted_pdf(new_window: editor.MainWindow, base_pdf: str) -> None:
     """Open a trivial pdf file."""
     new_window.file_path = base_pdf
-    file_reader = new_window.open_current_file()
+    file_reader = new_window.open_file(new_window.file_path)
     assert file_reader.metadata
     # PdfReader metadata attribute is accessible if the document is not encrypted
 
@@ -23,7 +23,7 @@ def test_open_encrypted_pdf_correct_password(
         QtWidgets.QInputDialog, "getText", lambda *args: (PASSWORD_BOTH, True)
     )
     new_window.file_path = encrypted_pdf_both
-    file_reader = new_window.open_current_file()
+    file_reader = new_window.open_file(new_window.file_path)
     assert file_reader.metadata
     # PdfReader metadata attribute is accessible if the document is successfully decrypted.
 
@@ -34,5 +34,5 @@ def test_dont_open_encrypted_pdf(
     """Fail to open an encrypted pdf file by not submitting a password."""
     monkeypatch.setattr(QtWidgets.QInputDialog, "getText", lambda *args: ("", False))
     new_window.file_path = encrypted_pdf_both
-    file_reader = new_window.open_current_file()
+    file_reader = new_window.open_file(new_window.file_path)
     assert file_reader is None
--- tests/test_generic.py
+++ tests/test_generic.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 from PyPDF2 import PdfReader, PdfMerger
 
 
@@ -7,17 +6,17 @@ def test_trivial() -> None:
     assert True
 
 
-def test_base_file(empty_pdf) -> None:
-    file_reader = PdfReader(empty_pdf)
-    assert os.path.isfile(empty_pdf)
+def test_base_file(base_pdf) -> None:
+    file_reader = PdfReader(base_pdf)
+    assert os.path.isfile(base_pdf)
     metadata = file_reader.metadata
     assert metadata["/Creator"] == "TeX"
-    with open(empty_pdf, "rb+") as f:
+    with open(base_pdf, "rb+") as f:
         file_merger = PdfMerger()
         file_merger.append(f)
         file_merger.add_metadata({"/Author": "Test_Author", "/Title": "Test_Title"})
         file_merger.write(f)
-    file_reader = PdfReader(empty_pdf)
+    file_reader = PdfReader(base_pdf)
     assert file_reader.metadata["/Author"] == "Test_Author"
 
 
